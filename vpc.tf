36f6f3cc16f53ad292fd21dee822de14d695e183
Pesheva Irina <pesheva.i@yandex.ru>
2024-12-22 12:38:20 +0300

    Change subnet name tempalte

--- vpc.tf
+++ vpc.tf
@@ -3,10 +3,10 @@ resource "yandex_vpc_network" "develop" {
 }
 
 resource "yandex_vpc_subnet" "develop" {
-  for_each   = { for index, subnet_info in var.vpc_subnets: index => subnet_info}
+  for_each = { for index, subnet_info in var.vpc_subnets : index => subnet_info }
 
-  name           = "${var.vpc_name}-${each.key}"
+  name           = "${var.vpc_name}-subnet-${each.key}"
   zone           = each.value.vpc_zone
   network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = [ each.value.vpc_cidr ]
+  v4_cidr_blocks = [each.value.vpc_cidr]
 }
